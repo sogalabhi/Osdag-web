00cb39605770cfe7581b99bc760b9333f72fa4b0
sogalabhi <abhijith.231cv203@nitk.edu.in>
2026-03-13 14:31:03 +0530

    removed legacy code

--- osdag_web/firebase_config.py
+++
@@ -1,8 +0,0 @@
-# firebase_config.py
-import firebase_admin
-from firebase_admin import credentials
-
-cred = credentials.Certificate("osdag_web/firebase-service-account.json")
-# Initialize only once
-if not firebase_admin._apps:
-    default_app = firebase_admin.initialize_app(cred)
