b02073552fe6a85a08b7f21fdfee95d128b2c240
sogalabhi <abhijith.231cv203@nitk.edu.in>
2026-05-27 17:01:47 +0530

    chore: add React 19 compatibility patch for Ant Design and configure Redis environment variable

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
+import '@ant-design/v5-patch-for-react-19';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
